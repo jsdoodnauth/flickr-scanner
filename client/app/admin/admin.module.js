75647544a50ddf6c4b3488cb7a93fdaa15b25d52
Joshua Doodnauth <josh.doodnauth@innovapost.com>
2016-04-05 13:48:16 -0400

    inital commit

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('flickrScannerApp.admin', [
+  'flickrScannerApp.auth',
+  'ui.router'
+]);
---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('flickrScannerApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
